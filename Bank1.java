71d7ff4aa298a3c95d3b313527d8541499ab027e
git2Fahim <35911014+gitFahim@users.noreply.github.com>
2019-04-16 11:01:30 +0600

    Add files via upload

---
+++ Bank1.java
@@ -0,0 +1,111 @@
+package hpack;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Bank1 {
+
+	private JFrame frame;
+	private JTextField UserName;
+	private JPasswordField password;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Bank1 window = new Bank1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Bank1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 15));
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bank System");
+		lblNewLabel.setFont(new Font("Bookman Old Style", Font.BOLD, 18));
+		lblNewLabel.setBounds(152, 11, 168, 22);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel Username = new JLabel("Username");
+		Username.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		Username.setBounds(10, 51, 97, 14);
+		frame.getContentPane().add(Username);
+		
+		UserName = new JTextField();
+		UserName.setBounds(162, 50, 86, 20);
+		frame.getContentPane().add(UserName);
+		UserName.setColumns(10);
+		
+		JLabel Password = new JLabel("Password");
+		Password.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		Password.setBounds(10, 80, 67, 14);
+		frame.getContentPane().add(Password);
+		
+		password = new JPasswordField();
+		password.setBounds(152, 81, 96, 20);
+		frame.getContentPane().add(password);
+		
+		JLabel lblAccountName = new JLabel("Account Name");
+		lblAccountName.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblAccountName.setBounds(10, 124, 97, 14);
+		frame.getContentPane().add(lblAccountName);
+		
+		JRadioButton SavingAccount = new JRadioButton("Saving Account");
+		SavingAccount.setBounds(152, 122, 109, 23);
+		frame.getContentPane().add(SavingAccount);
+		
+		JRadioButton DepositAccount = new JRadioButton("Deposit Account");
+		DepositAccount.setBounds(152, 148, 109, 23);
+		frame.getContentPane().add(DepositAccount);
+		
+		JButton Button = new JButton("Save");
+		Button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String Name = UserName.getText().toString();
+				String Password = password.getText().toString();
+				String Save = SavingAccount.getText().toString();
+				String Dep = DepositAccount.getText().toString();
+			
+			
+			try {
+				
+			}
+			catch(Exception e) {
+				
+			}
+			}
+		});
+		Button.setBounds(152, 198, 89, 23);
+		frame.getContentPane().add(Button);
+	}
+}
